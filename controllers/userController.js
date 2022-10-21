6d31f56289be63318c990409a2410541f4be57fa
rico4pepe <rico4pepe@gmail.com>
2022-10-21 17:13:58 +0100

    Creating backend with node- Setting up db connection and creation user model, and controller with jwt (Onboarding process)

---
+++ controllers/userController.js
@@ -0,0 +1,27 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken'
+
+import userModel from '../models/user.js'
+
+const secret = "test"
+
+export const signup = async(req, res)=>{
+    const {email, firstName, lastName, password} = req.body;
+    try {
+        const existingUser = await userModel.findOne({email})
+        if(existingUser){
+            return res.status(400).json({message: "User already exist"})
+        }
+
+        const hashpassword = await bcrypt.hash(password, 12)
+        const result = await userModel.create({
+            email,
+            password: hashpassword,
+            name: `${firstName} ${lastName}`
+        }) ;
+
+        const token = jwt.sign({email: result.email, id : result._id}, secret, {expiresIn: "1h"})
+    } catch (error) {
+        
+    }
+}
---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//Midddleware
+app.use(morgan("dev"));
+app.use(express.json({limit: "30mb", extended: "true"}))
+app.use(express.urlencoded({limit: "30mb", extended: "true"}))
+app.use(cors())
+
+//DB-URL
+const MONGODB_URL = "mongodb+srv://Rico:Nightg007@cluster0.ujpbbgr.mongodb.net/tour_db?retryWrites=true&w=majority";
+mongoose.connect(MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+//DB Connection
+
+console.log(mongoose.connection.readyState);
+
+console.log
+
+app.get('/', (req, res)=> {
+    res.send("Hello Express");
+});
+
+app.listen(port, () => console.log(`Listening on locahost: ${port}`));
